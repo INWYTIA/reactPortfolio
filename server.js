208f9b70e2358cbe1d730af80c77da46816d8a2d
INWYTIA <inwytia@gmail.com>
2020-05-18 18:19:05 -0500

    server error

--- server.js
+++ server.js
@@ -10,5 +10,5 @@ app.get('/ping', function (req, res) {
  return res.send('pong');
 });
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, '../build', 'index.html'));
+  res.sendFile(path.join(__dirname, './build', 'index.html'));
 });app.listen(port);
